cb1f823109da3d16b40281ce80b0ac04da32dd9d
Antonio Tapiador del Dujo <atd@singularities.org>
2017-04-05 11:41:20 +0200

    Basic code from tutorial

---
+++ client/imports/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import template from "./app.html";
+
+@Component({
+  selector: 'felicidades-marta',
+  template
+})
+export class FelicidadesMarta {}
---
+++ client/imports/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { FelicidadesMarta } from './app.component';
+
+@NgModule({
+  declarations: [
+    FelicidadesMarta
+  ],
+  entryComponents: [
+    FelicidadesMarta
+  ]
+})
+export class AppModule {}
