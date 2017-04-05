878be5dee17d7b683bf03451f64bb9479f1bb738
Antonio Tapiador del Dujo <atd@singularities.org>
2017-04-05 12:05:36 +0200

    Working with yarn and angular-material

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,12 +1,19 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';
 import { FelicidadesMarta } from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    MaterialModule
+  ],
   declarations: [
     FelicidadesMarta
   ],
   entryComponents: [
     FelicidadesMarta
-  ]
+  ],
+  bootstrap: [ FelicidadesMarta ]
 })
 export class AppModule {}
